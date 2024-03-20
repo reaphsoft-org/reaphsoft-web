d002cb160a2ac6240d6d5292fd3c6578d07603c7
Felix Sihitshuwam <felixsigit@gmail.com>
2024-03-20 12:48:33 +0100

    Added the text and buttons in the main body.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
         <NavBar />
+        <WriteUp />
     </main>
   );
 }
@@ -61,5 +62,36 @@ function NavBar() {
             </div>
         </div>
       </nav>
+    );
+}
+
+function WriteUp() {
+    return (
+        <div className="container">
+            <div className="my-4 my-lg-5"></div>
+            <div className="row">
+                <div className="col-12 col-md-7">
+                    <div className="text-white text-center sono-regular">
+                        <h2>Reaphsoft is an emerging business, with the focus to providing excellent software and security
+                            services to its clients across the globe.</h2>
+                        <h3 className="mt-5">We desire to work in partnership with all our clients, both local and international.</h3>
+                    </div>
+                </div>
+                <div className="col-12 col-md-5"></div>
+            </div>
+            <div className="my-4"></div>
+            <div className="row">
+                <div className="col-12 col-md-10">
+                    <div className="row text-center gy-4">
+                        <div className="col-12 col-md-4">
+                    <a className="home-btn text-white btn sono-regular">Software Dev</a>
+                </div>
+                        <div className="col-12 col-md-4">
+                    <a className="home-btn text-white btn sono-regular">IT Security</a>
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
