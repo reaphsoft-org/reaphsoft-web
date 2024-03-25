e09ea288649ce88f6467902da56a321b242ab845
Felix Sihitshuwam <felixsigit@gmail.com>
2024-03-25 07:01:16 +0100

    Working on security page, added a layout and fixed compatibility with app layout.

---
+++ src/app/security/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./styles.css";
+
+export const metadata: Metadata = {
+  title: "ReaphSoft Security",
+  description: "Created by kahlflekzy",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" data-bs-theme="dark">
+      <body>{children}</body>
+    </html>
+  );
+}
+
